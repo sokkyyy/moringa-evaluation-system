e6d2755a763c8914e9e9a24962006ae0583a3ac7
barakamulas <barakafrank18@gmail.com>
2020-01-14 16:00:06 +0300

    Change the app name on the navigation bar to lower case

--- mes-frontend/src/components/dashboard-navbar/Navbar.js
+++ mes-frontend/src/components/dashboard-navbar/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
 
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#" id="app_name">MES
+              <a className="nav-link" href="#" id="app_name">mes
                 <span className="sr-only">(current)</span>
               </a>
             </li> 
