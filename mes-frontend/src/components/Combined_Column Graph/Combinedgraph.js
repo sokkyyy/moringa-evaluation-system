dbb2ff4fcfeb320ae84839b101ca1d857722735f
barakamulas <barakafrank18@gmail.com>
2020-01-15 09:38:47 +0300

    Return the CanvasJSChart that will be generated inside a div in the return method of CombineLineGraph

--- mes-frontend/src/components/Combined_Column Graph/Combinedgraph.js
+++ mes-frontend/src/components/Combined_Column Graph/Combinedgraph.js
@@ -99,13 +99,13 @@ class CombinedLineGraph extends Component {
 		}
     return ( 
 
-    //   <div>
+      <div>
 			
-		// 	<CanvasJSChart options = {options} 
-		// 		// onRef={ref => this.chart = ref}
-		// 	/>
-		// 	{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		// </div>
+			<CanvasJSChart options = {options} 
+				onRef={ref => this.chart = ref}
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
 
      );
   }
