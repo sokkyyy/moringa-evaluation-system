52a1a03a5389f03ac182d3315ddc1266072a05bf
barakamulas <barakafrank18@gmail.com>
2020-01-15 17:10:19 +0300

    Create Clock component inside a Clock folder in components

---
+++ mes-frontend/src/components/Clock/Clock.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Clock
+ extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Clock
+;
--- mes-frontend/src/components/Countdown_Timer/CountdownTimer.js
+++ mes-frontend/src/components/Countdown_Timer/CountdownTimer.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 class CountdownTimer extends Component {
-  state = {  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'January, 17, 2020'
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: newDeadline})
+  }
+  
   render() { 
-    return (  );
+    return ( 
+      <div className="timer">
+        <div className="timer-title">
+          Countdown to {this.state.deadline}
+        </div>
+        <div>
+          <div className="clock-days">2 days</div>
+          <div className="clock-hours">12 hours</div>
+          <div className="clock-minutes">15 minutes</div>
+          <div className="clock-seconds">28 seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date" onChange ={event => this.setState[{newDeadline: event.target.value}]}/>
+          <button onClick={() => this.changeDeadline()}>Submit</button>
+        </div>
+      </div>
+      );
   }
 }
  
