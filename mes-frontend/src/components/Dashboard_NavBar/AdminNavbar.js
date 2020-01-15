e38c3c95fe53c5ef4e23b289b103d48a74544814
barakamulas <barakafrank18@gmail.com>
2020-01-16 00:01:33 +0300

    Create an AdminNavbar component in Admin Navbar that will be used to Navbar for the Admin Dashboard which will be created in AdminDashboard component in Admin_Dashboard

---
+++ mes-frontend/src/components/Dashboard_NavBar/AdminNavbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AdminNavbar extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div></div>
+     );
+  }
+}
+ 
+export default AdminNavbar;
