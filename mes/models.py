1931050f63d0e9fd0c8fc17a9c46b3bb277f38dc
tumaa <ramanfatuu@gmail.com>
2020-01-15 19:40:47 +0300

    created a class to grade the staff members

--- mes/models.py
+++ mes/models.py
@@ -12,5 +12,21 @@ class Role(models.Model):
     role = models.CharField(max_length=20,choices=role_choices)
     def __str__(self):
         return self.role
-
+class JobGrade(models.Model):
+    '''Model for Job Grades'''
+    moringa_job_grade_choices = [
+        ('12','12'), #champion
+        ('11','11'), #experts
+        ('10','10'),
+        ('9','9'), #practitioner
+        ('8','8'),
+        ('7','7'),
+        ('6','6'),
+        ('5','5'),#beginner
+        ('4','4'),
+        ('1','1'),
+    ]
+    grade = models.CharField(max_length=10, choices=moringa_job_grade_choices,)
+    def __str__(self):
+        return self.grade
     
