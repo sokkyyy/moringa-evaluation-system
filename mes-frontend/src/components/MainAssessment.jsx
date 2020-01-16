1a3c2976bb8c8cadb2648a27f49a51db27e86eab
cynthiaoduol <cynthiaobu940@gmail.com>
2020-01-16 10:12:45 +0300

    create assessment forms for beginner level

--- mes-frontend/src/components/MainAssessment.jsx
+++ mes-frontend/src/components/MainAssessment.jsx
@@ -1,18 +1,140 @@
-import React, { Component } from 'react';
-import Organization from './Organization';
-import Communication from './Communication';
-import CriticalThinking from './CriticalThinking';
-import BuildRelationship from './BuildRelationship';
-import Innovation from './Innovation';
-
+import React, { Component } from "react";
+import Organization from "./Organization";
+import Innovation from "./Innovation";
+import Communication from "./Communication";
+import CriticalThinking from "./CriticalThinking";
+import BuildRelationship from "./BuildRelationship";
+import Success from "./Success";
 
 class MainAssessment extends Component {
-  state = {  }
-  render() { 
-    return (
-      <h1>Yes</h1>
-      );
+  state = {
+    step: 1,
+    planning: "",
+    execution: "",
+    prioritization: "",
+    visionSetting: "",
+    thinking: "",
+    adaptability: "",
+    buildInvestment: "",
+    effectiveComm: "",
+    delivering: "",
+    compilation: "",
+    analysis: "",
+    problemSolving: "",
+    improvement: "",
+    teamwork: "",
+    stakeholder: "",
+    conflicts: ""
+  };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  handleChange = input => event => {
+    this.setState({ [input]: event.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const {
+      planning,
+      execution,
+      prioritization,
+      visionSetting,
+      thinking,
+      adaptability,
+      buildInvestment,
+      effectiveComm,
+      delivering,
+      compilation,
+      analysis,
+      problemSolving,
+      improvement,
+      teamwork,
+      stakeholder,
+      conflicts
+    } = this.state;
+
+    const values = {
+      planning,
+      execution,
+      prioritization,
+      visionSetting,
+      thinking,
+      adaptability,
+      buildInvestment,
+      effectiveComm,
+      delivering,
+      compilation,
+      analysis,
+      problemSolving,
+      improvement,
+      teamwork,
+      stakeholder,
+      conflicts
+    };
+
+    switch (step) {
+      case 1:
+        return (
+          <Organization
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <Innovation
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Communication
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 4:
+        return (
+          <CriticalThinking
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 5:
+        return (
+          <BuildRelationship
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+
+      case 6:
+        return <Success />;
+    }
   }
 }
- 
+
 export default MainAssessment;
---
+++ mes-frontend/src/components/Success.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Success extends Component {
+  state = {  }
+  render() { 
+    return (  
+      <div className="text-center">
+        <h4>Thank you for taking the Assessment.</h4>
+        <h3> You can view your results in your dashboard</h3>
+      </div>
+    );
+  }
+}
+ 
+export default Success;
