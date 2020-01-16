e9d986da1b9b5baf2d2d22daae16518febbe47a0
sokkyyy <ndegwaraymond@gmail.com>
2020-01-16 11:31:36 +0300

    Added the routing for the login page and home page on App.js

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Login from './components/auth/Login';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-    rrr
-    </div>
+    <BrowserRouter>
+      <Route path='/login' exact component={Login}></Route>
+      <Route path='/home' exact component={Home}></Route>
+    </BrowserRouter>
   );
 }
 
