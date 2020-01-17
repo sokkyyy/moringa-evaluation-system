69ff4192cb36d64d68adbd254d6a26d26df700db
barakamulas <barakafrank18@gmail.com>
2020-01-17 10:01:06 +0300

    Pull preparations

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path='/' component={Dashboard}/>
+          <Route path='/' component={AdminDashboard}/>
         </Switch>      
       </div>
     </BrowserRouter>
