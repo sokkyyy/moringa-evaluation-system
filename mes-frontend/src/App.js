f80e342e8ce93034fd33d4aa4f0330cab58438b0
sokkyyy <ndegwaraymond@gmail.com>
2020-01-21 08:27:22 +0300

    Successfully connected the landing/login page with the dashboard. A bug prevented the use of react router dom for seemless navigation.

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -2,23 +2,23 @@
 
 import Dashboard from './components/Dashboard/Dashboard';
 import Home from './components/Home';
-import Login from './components/auth/Login';
 import React from 'react';
 import './App.css';
-import MainAssessment from './components/Assessment/MainAssessment'
+import MainAssessment from './components/Assessment/MainAssessment';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Landing from './components/Landing/Landing'
-import Footer from './components/Footer/Footer'
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
+import Footer from './components/Footer/Footer';
+import history from './history';
 
 function App() {
   return (
 
-    <BrowserRouter>
-      <Route path='/login' exact component={Login}></Route>
-      <Route path='/dashboard' exact component={Dashboard}></Route>
-      <Route path='/' component={Landing}/>
+    <BrowserRouter history={history}>
+      <Switch>
+        <Route path='/dashboard' exact component={Dashboard}></Route>
+        <Route path='/' component={Landing} />
+      </Switch>
     </BrowserRouter>
   );
 }
