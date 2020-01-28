f05027d2ecbe62b6f9f5f1467e8e1e9d4ebc11a9
cynthiaoduol <cynthiaobu940@gmail.com>
2020-01-28 14:56:47 +0300

    assessment

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -1,16 +1,16 @@
 
-import Dashboard from './components/Dashboard/Dashboard';
-import Home from './components/Home';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import Home from './components/Home';
 import React from 'react';
 import './App.css';
 import MainAssessment from './components/Assessment/MainAssessment'; //SELF
-import ManagerAssessment from './components/ManagerAssessment/ManagerAssessment'; //MANAGER
+// import ManagerAssessment from './components/ManagerAssessment/ManagerAssessment'; //MANAGER
 
 
-import AdminDashboard from './components/Dashboard/AdminDashboard';
+// import AdminDashboard from './components/Dashboard/AdminDashboard';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Landing from './components/Landing/Landing';
-import Footer from './components/Footer/Footer';
+// import Landing from './components/Landing/Landing';
+// import Footer from './components/Footer/Footer';
 import history from './history';
 
 function App() {
@@ -20,11 +20,13 @@ function App() {
 
     <BrowserRouter history={history}>
       <Switch>
-        <Route path='/dashboard' exact component={Dashboard}></Route>
-        <Route path='/' exact component={Landing} />
-        <Route path='/assessment' exact component={MainAssessment} />
-        <Route path='/manager/assessment' exact component={ManagerAssessment} />
-        <Route path='/admin/dashboard' exact component={AdminDashboard} />
+        {/* <Route path='/dashboard' exact component={Dashboard}></Route>
+        <Route path='/' exact component={Landing} /> */}
+        {/* <Route path='/assessment' exact component={MainAssessment} /> */}
+
+        <Route path='/' component={MainAssessment} />
+        {/* <Route path='/manager/assessment' exact component={ManagerAssessment} />
+        <Route path='/admin/dashboard' exact component={AdminDashboard} /> */}
 
       </Switch>
 
--- mes-frontend/src/components/Assessment/Organization.jsx
+++ mes-frontend/src/components/Assessment/Organization.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Form, Button } from "semantic-ui-react";
 
 class Organization extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
@@ -13,7 +13,7 @@ class Organization extends Component {
     this.props.nextStep();
   };
 
-  handleChange(event){
+  handleChange(event) {
     this.props.handleChange(event);
   }
   back = e => {
@@ -34,93 +34,141 @@ class Organization extends Component {
                 executing, and prioritizing effectively and consistently.
               </p>
 
-          <div className="planning1">
-            <h3>Planning</h3>
-            <h5>Plan tasks effectively and consistently within a project</h5>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.planning" value="1" onChange={this.handleChange} />
-                Struggles to plan effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.planning" value="2" onChange={this.handleChange} />
-                Plans effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.planning" value="2" onChange={this.handleChange} />
-                Excels at planning effectively and consistently
-              </label>
+              <div className="planning1">
+                <h3>Planning</h3>
+                <h5>
+                  Plan tasks effectively and consistently within a project
+                </h5>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.planning"
+                      value="1"
+                      onChange={this.handleChange}
+                    />
+                    Struggles to plan effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.planning"
+                      value="2"
+                      onChange={this.handleChange}
+                    />
+                    Plans effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.planning"
+                      value="2"
+                      onChange={this.handleChange}
+                    />
+                    Excels at planning effectively and consistently
+                  </label>
+                </div>
+              </div>
+            </Form>
+
+            <Form>
+              <div className="execution1">
+                <h3>Execution</h3>
+                <h5>
+                  Executes tasks effectively and consistently within a project
+                </h5>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.execution"
+                      value="1"
+                      onChange={this.handleChange}
+                    />
+                    Struggles to execute effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.execution"
+                      value="2"
+                      onChange={this.handleChange}
+                    />
+                    Executes effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.execution"
+                      value="3"
+                      onChange={this.handleChange}
+                    />
+                    Excels at executing effectively and consistently
+                  </label>
+                </div>
+              </div>
+            </Form>
+
+            <Form>
+              <div className="prioritization1">
+                <h3>Prioritization</h3>
+                <h5>
+                  Prioritizes tasks effectively and consistently within a
+                  project
+                </h5>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.prioritization"
+                      value="1"
+                      onChange={this.handleChange}
+                    />
+                    Struggles to prioritize effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.prioritization"
+                      value="2"
+                      onChange={this.handleChange}
+                    />
+                    Prioritizes effectively and consistently
+                  </label>
+                </div>
+                <div className="custom-control custom-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="organization.prioritization"
+                      value="3"
+                      onChange={this.handleChange}
+                    />
+                    Excels at prioritizing effectively and consistently
+                  </label>
+                </div>
+              </div>
+            </Form>
+
+            <div className="text-center">
+              <Button onClick={this.back} className="previous">
+                Back
+              </Button>
+              <Button onClick={this.saveAndContinue} className="next">
+                Continue
+              </Button>
             </div>
           </div>
-        </Form>
-
-        <Form>
-          <div className="execution1">
-            <h3>Execution</h3>
-            <h5>
-              Executes tasks effectively and consistently within a project
-            </h5>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.execution" value="1" onChange={this.handleChange} />
-                Struggles to execute effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.execution" value="2" onChange={this.handleChange} />
-                Executes effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.execution" value="3" onChange={this.handleChange} />
-                Excels at executing effectively and consistently
-              </label>
-            </div>
-          </div>
-        </Form>
-
-        <Form>
-          <div className="prioritization1">
-            <h3>Prioritization</h3>
-            <h5>
-              Prioritizes tasks effectively and consistently within a project
-            </h5>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.prioritization" value="1" onChange={this.handleChange} />
-                Struggles to prioritize effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.prioritization" value="2" onChange={this.handleChange} />
-                Prioritizes effectively and consistently
-              </label>
-            </div>
-            <div className="custom-control custom-radio">
-              <label>
-                <input type="radio" name="organization.prioritization" value="3" onChange={this.handleChange} />
-                Excels at prioritizing effectively and consistently
-              </label>
-            </div>
-          </div>
-          </Form>
-
-          <div className="text-center">
-            <Button onClick={this.back} className="previous">
-              Back
-            </Button>
-            <Button onClick={this.saveAndContinue} className="next">
-              Continue
-            </Button>
-          </div>
-
         </fieldset>
       </div>
     );
